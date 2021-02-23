a3223c1743579d4ad8bf10eb4ca05600905c4f4f
Khush Patel <khuskhushi99@gmail.com>
2021-02-22 21:21:40 -0500

    Project outline complete

--- amazing-watchlist/src/app/app.module.ts
+++ amazing-watchlist/src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchComponent } from './search/search.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { ShowInfoComponent } from './show-info/show-info.component';
+import { ShowDetailsComponent } from './show-details/show-details.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    SearchResultsComponent,
+    ShowInfoComponent,
+    ShowDetailsComponent,
+    WatchlistComponent
   ],
   imports: [
     BrowserModule,
