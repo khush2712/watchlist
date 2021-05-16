94773202f3a09f820a3cf2fb1d666c70b37ed7c6
Khush Patel <khuskhushi99@gmail.com>
2021-05-16 13:33:21 -0400

    first live version

---
+++ src/app/controls/htmlstripper.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HtmlstripperPipe } from './htmlstripper.pipe';
+
+describe('HtmlstripperPipe', () => {
+  it('create an instance', () => {
+    const pipe = new HtmlstripperPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  query = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    if (this.query.length === 0) {
+      return;
+    }
+  }
+
+  search(): void {
+    this.router.navigate(  ['search', this.query]);
+  }
+}
